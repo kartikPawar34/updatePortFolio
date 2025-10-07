2af50ceed1c8c67aa89283a24b7787f170dd4fa5
KARTIK  PAWAR <kp346949@gmail.com>
2025-10-07 13:48:07 +0530

    fix: Update vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 💥 ADDED THIS LINE TO FIX BLANK PAGE ISSUE ON GITHUB PAGES
-  base: "/Kartik-Pawar-2nd-Portfolio-/", 
+  base: "./", 
   plugins: [react()],
 })
