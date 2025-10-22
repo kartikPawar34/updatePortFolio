b7d01b083c814981c43eab67c07ccde900b1b655
KARTIK  PAWAR <kp346949@gmail.com>
2025-10-22 13:25:35 +0530

    certification added

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,9 +1,10 @@
 function Projects(){
   return (
     <>
-      <div className="card">
+      <div className="card-1">
         <p class="cardsBlock">Developed a digital image processing web application called Kreate—a lightweight tool for basic image manipulation. Capabilities include background removal, cropping, sharpening, and other enhancements using core image processing algorithms.
-        Visit</p>
+        </p>
+        <a href="https://www.canva.com/design/DAG2f7yaub0/PQxMlC89x6C2X7Bc-U4kdQ/view?utm_content=DAG2f7yaub0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=he1d51e8f04"><button>Visit</button></a>
       </div>
     </>
   )
