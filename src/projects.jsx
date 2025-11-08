71c1af7ebef0959f3d6d7da470429d94f7fd795e
Kartik Pawar <kp346949@gmail.com>
2025-11-08 17:35:36 +0530

    remove buttons
    Removed text from the button in the project link.

--- src/projects.jsx
+++ src/projects.jsx
@@ -4,7 +4,7 @@ function Projects(){
       <div className="card-1">
         <p class="cardsBlock">Developed a digital image processing web application called Kreate—a lightweight tool for basic image manipulation. Capabilities include background removal, cropping, sharpening, and other enhancements using core image processing algorithms.
         </p>
-        <a href="https://www.canva.com/design/DAG2f7yaub0/PQxMlC89x6C2X7Bc-U4kdQ/view?utm_content=DAG2f7yaub0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=he1d51e8f04"><button>Visit</button></a>
+        <a href="https://www.canva.com/design/DAG2f7yaub0/PQxMlC89x6C2X7Bc-U4kdQ/view?utm_content=DAG2f7yaub0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=he1d51e8f04"><button></button></a>
       </div>
     </>
   )
