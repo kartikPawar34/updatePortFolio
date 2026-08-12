79be8b0d9615fde2fcfa6157df62028d99497048
KARTIK  PAWAR <kp346949@gmail.com>
2026-08-12 17:11:15 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Use relative path for all assets
-  base: './', 
+  base: '/updatePortFolio/',
   plugins: [react()],
 })
